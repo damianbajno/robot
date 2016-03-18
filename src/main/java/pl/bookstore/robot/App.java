263653469104e877092507f10a22b0609d8fa898
Adam Lasota <adam_lasota@epam.com>
2016-03-18 14:47:18 +0100

    Robot do wyszukiwania ksiazek

---
+++ src/main/java/pl/bookstore/robot/App.java
@@ -0,0 +1,14 @@
+package pl.bookstore.robot;
+
+import org.apache.log4j.Logger;
+
+public class App 
+{
+ 
+	final static Logger logger = Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	logger.error("asdasd");
+    	System.out.println("sdasd");
+    }
+}
