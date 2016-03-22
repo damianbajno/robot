b4b609bc664b53d81f65c8f3f13e8080cb1e4a9a
Adam <adam_lasota@epam.com>
2016-03-22 10:33:05 +0100

    Dodanie Klasy ktora czyta promocyjne ksiazki z serwisu pwn ksiegarnia.pwn.pl/";
    Kolejnym krokiem moim bedzie dodanie funkcji czytania ksiazek z serwisu
    matras.pl

---
+++ src/main/java/pl/bookstore/robot/Main.java
@@ -0,0 +1,21 @@
+package pl.bookstore.robot;
+
+import org.apache.log4j.Logger;
+
+import com.jaunt.NotFound;
+import com.jaunt.ResponseException;
+
+import getbooks.Pwn;
+
+public class Main {
+
+	final static Logger logger = Logger.getLogger(Main.class);
+
+	public static void main(String[] args) throws NotFound, ResponseException {
+		logger.info("Robot Start");
+
+		Pwn pwd = new Pwn();
+		System.out.println(pwd);
+
+	}
+}
